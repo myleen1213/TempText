88e7437afff64e3fc5ffc39d9f5e5d033bca0eb9
Ayana Bando <ayanaflax@yahoo.com>
2022-02-01 20:08:26 -0500

    add api package and class

---
+++ app/src/main/java/com/example/android/temptext/network/WeatherApiService.kt
@@ -0,0 +1,39 @@
+package com.example.android.temptext.network
+
+package com.example.android.temptext.network
+
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.GET
+
+private const val BASE_URL = "https://api.weatherapi.com/v1/"
+
+
+private val networkLoggingInterceptor =
+    HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+private val moshi = Moshi.Builder()
+    .add(KotlinJsonAdapterFactory())
+    .build()
+
+private val retrofit = Retrofit.Builder()
+    .client(OkHttpClient.Builder().addInterceptor(networkLoggingInterceptor).build())
+    .addConverterFactory(MoshiConverterFactory.create(moshi))
+    .baseUrl(BASE_URL)
+    .build()
+
+interface WeatherApiService {
+    ///alerts/active/area/{area}"
+    @GET("current.json?key=$WEATHER_KEY&q=NEW York&aqi=no")
+    suspend fun getCurrentWeather(): WeatherAlert
+}
+
+object WeatherAlertApi{
+    val retrofitService: WeatherApiService by lazy {
+        retrofit.create((WeatherApiService::class.java))
+    }
+}
